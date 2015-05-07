5566e817a65f72064f943feb637dae7e70b15332
Neroist <wbolin@kth.se>
2015-05-07 14:28:24 +0200

    window XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD

---
+++ src/Andin.java
@@ -0,0 +1,21 @@
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.paint.Color;
+import javafx.scene.media.*;
+
+
+
+public class Andin extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+	@Override
+	public void start(Stage primaryStage){
+		Group group = new Group();
+		primaryStage.show();
+	}
+
+}
