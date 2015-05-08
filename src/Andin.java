62e040aa577e53e051d42ba2959e6450f67d374d
Neroist <wbolin@kth.se>
2015-05-09 00:53:37 +0200

    Grovt kodskal, lite test

--- src/Andin.java
+++ src/Andin.java
@@ -1,21 +1,45 @@
 import javafx.application.*;
 import javafx.scene.*;
 import javafx.stage.*;
+import javafx.event.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.media.*;
-
-
+import java.util.*;
 
 public class Andin extends Application{
-
+	
+	public MediaPlayer mediaplayer; //Huvudspelaren, uppdateras vartefter.
+	public ArrayList<Media> queue;	//Lista med media som skall spelas upp.
 	public static void main(String[] args) {
-		launch(args);
+		launch(args); // Ladda applikationen
 
 	}
+	// TODO: Play/pause/stop etc görs antagligen via lambdafunktioner bundna till knappar
 	@Override
 	public void start(Stage primaryStage){
-		Group group = new Group();
+		
+		// INITIAL TESTNING
+		StackPane layout = new StackPane();
+		
+		Button playButton = new Button("SHIEEET");
+		playButton.setOnAction(e -> System.out.println("IT WORKS XDD TOP LEL OMG LE TROLEFACE"));
+		
+		layout.getChildren().add(playButton);
+		Scene scene = new Scene(layout, 400,400, Color.BLACK);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
+	public void addPlaylist(){
+		//TODO 
+	}
+	public void removePlaylist(String name){
+		//TODO
+	}
+	public void queueMedia(){
+		//TODO
+	}
+	
 
 }
---
+++ src/Playlist.java
@@ -0,0 +1,20 @@
+
+public class Playlist {
+	
+	public static void newPlaylist(String name){
+		//TODO
+	}
+	public static void addMedia(String name){
+		//TODO
+	}
+	public static void removeMedia(String name){
+		//TODO
+	}
+	public static void renamePlaylist(String name){
+		//TODO
+	}
+	public static void deletePlaylist(String name){
+		
+	}
+	
+}
