456ed6bebf3e000f440a53c8a74d72ec90c85818
Neroist <wbolin@kth.se>
2015-05-09 13:10:45 +0200

    menu and playbuttons

--- src/Andin.java
+++ src/Andin.java
@@ -21,16 +21,63 @@ public class Andin extends Application{
 	@Override
 	public void start(Stage primaryStage){
 		
+		
+		mediaplayer = new MediaPlayer(new Media("file:///C:/Users/william/Music/shit.mp3"));
+		
+		
 		// INITIAL TESTNING
-		Group group = new Group();
+		VBox box = new VBox();
 		
-		Button playButton = new Button("SHIEEET");
-		playButton.setOnAction(e -> System.out.println("IT WORKS XDD TOP LEL OMG LE TROLEFACE"));
 		
-		group.getChildren().add(playButton);
 		
-		Scene scene = new Scene(group, 400,400, Color.CORNFLOWERBLUE);
 		
+		// Meny
+		MenuBar menuBar = new MenuBar();
+        Menu menuFile = new Menu("File");
+        Menu menuPlaylist = new Menu("Playlists");
+        
+        MenuItem playMedia = new MenuItem("Play media");
+        MenuItem queueMedia = new MenuItem("Queue media");
+
+        MenuItem newPlaylist = new MenuItem("New playlist");
+        MenuItem playPlaylist = new MenuItem("Queue media");
+        MenuItem queuePlaylist = new MenuItem("Queue playlist");
+        
+        menuFile.getItems().addAll(playMedia, queueMedia);
+        menuPlaylist.getItems().addAll(newPlaylist, playPlaylist, queuePlaylist);
+        
+        
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showOpenDialog(primaryStage);
+        
+        
+        menuBar.getMenus().addAll(menuFile, menuPlaylist);
+        
+        
+        
+        box.getChildren().add(menuBar);
+		
+        
+        
+        
+        
+        //Huvudknappar
+        Button playButton = new Button("Play");
+		playButton.setOnAction(e -> mediaplayer.play());
+		box.getChildren().add(playButton);
+		Button pauseButton = new Button("Pause");
+		pauseButton.setOnAction(e -> mediaplayer.pause());
+		box.getChildren().add(pauseButton);
+		Button stopButton = new Button("Stop");
+		stopButton.setOnAction(l -> mediaplayer.stop());
+		box.getChildren().add(stopButton);
+        
+		
+		
+		
+		
+		Scene scene = new Scene(box, 400,400, Color.CORNFLOWERBLUE);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
