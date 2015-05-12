c7516e4bb2ce01f35743d8263d11ef0300fb2b46
Neroist <wbolin@kth.se>
2015-05-12 13:39:16 +0200

    qweqwe

--- src/Andin.java
+++ src/Andin.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import javafx.application.*;
 import javafx.scene.*;
 import javafx.stage.*;
@@ -8,10 +9,12 @@ import javafx.scene.paint.Color;
 import javafx.scene.media.*;
 import java.util.*;
 
+import java.lang.*;
+
 public class Andin extends Application{
-	
-	public MediaPlayer mediaplayer; //Huvudspelaren, uppdateras vartefter.
-	public ArrayList<Media> queue;	//Lista med media som skall spelas upp.
+	public FileChooser fileChooser;
+	public MediaView mediaView;
+	public LinkedList<Media> queue;	//Lista med media som skall spelas upp.
 	public Media currentMedia; 		//Nuvarande media
 	public static void main(String[] args) {
 		launch(args); // Ladda applikationen
@@ -20,16 +23,15 @@ public class Andin extends Application{
 	// TODO: Play/pause/stop etc görs antagligen via lambdafunktioner bundna till knappar
 	@Override
 	public void start(Stage primaryStage){
-		
-		
-		mediaplayer = new MediaPlayer(new Media("file:///C:/Users/william/Music/shit.mp3"));
+		fileChooser = new FileChooser();
+		queue = new LinkedList<Media>();
 		
 		
 		// INITIAL TESTNING
 		VBox box = new VBox();
 		
-		
-		
+		mediaView = new MediaView();
+		box.getChildren().add(mediaView);
 		
 		// Meny
 		MenuBar menuBar = new MenuBar();
@@ -37,19 +39,31 @@ public class Andin extends Application{
         Menu menuPlaylist = new Menu("Playlists");
         
         MenuItem playMedia = new MenuItem("Play media");
+        playMedia.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if (file != null) {
+                	
+                	Media m = new Media(file.toURI().toString());
+                	queue.add(m);
+                	initMediaPlayer();
+                }
+            }
+        });
+        
         MenuItem queueMedia = new MenuItem("Queue media");
 
         MenuItem newPlaylist = new MenuItem("New playlist");
         MenuItem playPlaylist = new MenuItem("Queue media");
         MenuItem queuePlaylist = new MenuItem("Queue playlist");
         
+        
+        
         menuFile.getItems().addAll(playMedia, queueMedia);
         menuPlaylist.getItems().addAll(newPlaylist, playPlaylist, queuePlaylist);
-        
-        
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
-        fileChooser.showOpenDialog(primaryStage);
+       
+      
         
         
         menuBar.getMenus().addAll(menuFile, menuPlaylist);
@@ -60,26 +74,27 @@ public class Andin extends Application{
 		
         
         
-        
-        
         //Huvudknappar
         Button playButton = new Button("Play");
-		playButton.setOnAction(e -> mediaplayer.play());
+		playButton.setOnAction(e -> mediaView.getMediaPlayer().play());
 		box.getChildren().add(playButton);
 		Button pauseButton = new Button("Pause");
-		pauseButton.setOnAction(e -> mediaplayer.pause());
+		pauseButton.setOnAction(e -> mediaView.getMediaPlayer().pause());
 		box.getChildren().add(pauseButton);
 		Button stopButton = new Button("Stop");
-		stopButton.setOnAction(l -> mediaplayer.stop());
+		stopButton.setOnAction(l -> mediaView.getMediaPlayer().stop());
 		box.getChildren().add(stopButton);
         
-		
-		
+        
 		
 		
 		Scene scene = new Scene(box, 400,400, Color.CORNFLOWERBLUE);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		
+		
+		
 	}
 	public void addPlaylist(){
 		//TODO 
@@ -90,6 +105,18 @@ public class Andin extends Application{
 	public void queueMedia(){
 		//TODO
 	}
-	
-
+	private void initMediaPlayer(){
+	    if (!queue.isEmpty()){
+	    	Media m = queue.pop();
+	        MediaPlayer mediaPlayer = new MediaPlayer(m);
+	        mediaPlayer.setOnEndOfMedia(new Runnable() {
+	            @Override public void run() {
+	                initMediaPlayer();
+	            }
+	        });
+	        mediaView.setMediaPlayer(mediaPlayer);
+	    }
+	    
+	    
+	}
 }
