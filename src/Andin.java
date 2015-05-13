d8660e38de1465444f539ea055b8793bed61a140
Neroist <wbolin@kth.se>
2015-05-13 14:38:19 +0200

    asd

--- src/Andin.java
+++ src/Andin.java
@@ -21,7 +21,6 @@ public class Andin extends Application{
 	}
 	@Override
 	public void start(Stage primaryStage){
-		String unnecessary;
 		// Actual stuff
 		fileChooser = new FileChooser();
 		queue = new LinkedList<Media>();
