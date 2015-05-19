1651436802534190a10646a381bb6d6b6e989787
Sam Andren <samand@kth.se>
2015-05-19 22:31:15 +0200

    Displayplaylist
    MakePlaylistBox called with reference to mediaPane

--- src/Andin.java
+++ src/Andin.java
@@ -1,14 +1,18 @@
 import java.io.*;
+
 import javafx.collections.*;
 import javafx.application.*;
 import javafx.scene.*;
 import javafx.stage.*;
 import javafx.event.*;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.media.*;
+
 import java.util.*;
+
 import javafx.scene.input.*;
 
 public class Andin extends Application{
@@ -19,15 +23,14 @@ public class Andin extends Application{
 	public MediaView mediaView;		//Global vy fÃ¶r all media
 	public LinkedList<Media> queue;	//Lista med media som skall spelas upp.
 	public ListView<String> playlistView;//vyn for spellistor
+	public ListView<String> playlistContentsView; //Vyn för spellistors innehåll
 
-
-
+	
 	public static void main(String[] args) {
 		launch(args); // Ladda applikationen
-
 	}
-
-
+	
+	
 	@Override
 	public void start(Stage stage){
 		primaryStage = stage;
@@ -37,19 +40,18 @@ public class Andin extends Application{
 		mediaView = new MediaView();
 		playlistHandler = new PlaylistHandler();
 		playlistHandler.setXMLSource("C:/testljud/playlists.xml");
-
+		playlistContentsView = new ListView<String>();
 
 		BorderPane borderPane = new BorderPane();
-
-
 
 		nowPlaying = new Label();
 		nowPlaying.setText("No media playing.");
 
 		HBox buttonbox = makeButtons();
 		MenuBar menuBar = makeMenuBar();
-		VBox playlistBox = makePlaylistBox();
+
 		StackPane mediaPane = makeMediaPane();
+		VBox playlistBox = makePlaylistBox(mediaPane);
 
 		BorderPane bottomPane = new BorderPane();
 		bottomPane.setLeft(buttonbox);
@@ -59,15 +61,9 @@ public class Andin extends Application{
 		borderPane.setTop(menuBar);
 		borderPane.setBottom(bottomPane);
 		borderPane.setLeft(playlistBox);
-
 		Scene scene = new Scene(borderPane, 1920,1080, Color.CORNFLOWERBLUE);
-
 		primaryStage.setScene(scene);
 		primaryStage.show();
-
-
-
-
 	}
 	private void initMediaPlayer(){
 		if (!queue.isEmpty()){
@@ -80,7 +76,9 @@ public class Andin extends Application{
 					initMediaPlayer();
 				}
 			});
-			nowPlaying.setText(m.getSource().replaceAll("(.*/)+", ""));
+			String mediaName =m.getSource().replaceAll("(.*/)+", "Now playing: ");
+//			mediaName = mediaName.replaceAll("\\..*", "");
+			nowPlaying.setText(mediaName);
 			mediaView.setMediaPlayer(mediaPlayer);
 
 		}else{
@@ -159,11 +157,44 @@ public class Andin extends Application{
 		menuBar.getMenus().add(menuFile);
 		return menuBar;
 	}
-	private VBox makePlaylistBox(){
+	private void showPlaylistContents(String playlist, VBox playlistContentsBox){
+		ObservableList<String> playlistContents =FXCollections.observableArrayList();
+		String[] mediaPaths = playlistHandler.getMediaPaths(playlist);
+		for (String s: mediaPaths){
+			s =s.replaceAll("(.*/)+", "");
+			s = s.replaceAll("\\..*", "");
+			playlistContents.add(s);
+		}
+		playlistContentsView.setItems(playlistContents);
+		
+	}
+	private VBox makeShowPlaylistContentsBox(){
+
+		VBox playlistContentsBox = new VBox();
+		playlistContentsBox.setVisible(false);
+		Button hideplaylistContentsBoxButton = new Button("<<< Hide tracks");		
+		playlistContentsBox.setVgrow(playlistContentsView, Priority.ALWAYS);
+		hideplaylistContentsBoxButton.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent event){
+				playlistContentsBox.setVisible(false);
+			}
+		});
+		playlistContentsBox.getChildren().add(hideplaylistContentsBoxButton);
+		playlistContentsBox.getChildren().add(playlistContentsView);
+		
+		return playlistContentsBox;
+	}
+	private VBox makePlaylistBox(StackPane mediaPaneRef){
 		VBox playlistBox = new VBox();
 		AnchorPane aPane = new AnchorPane();
 		ContextMenu contextMenu = new ContextMenu();
 		ObservableList<String> playlists =FXCollections.observableArrayList();
+		
+		VBox playlistContentsBox = makeShowPlaylistContentsBox();
+		mediaPaneRef.getChildren().add(playlistContentsBox);
+		playlistContentsBox.setMaxWidth(300);
+		mediaPaneRef.setAlignment(playlistContentsBox,Pos.TOP_LEFT);
 
 
 		String[] playlistStrings=playlistHandler.getPlaylists();
@@ -171,7 +202,6 @@ public class Andin extends Application{
 			playlists.add(s);
 		}
 		playlistView.setItems(playlists);
-
 		Button newPlaylistButton = new Button("New Playlist");
 		MenuItem playPlaylistOption = new MenuItem("Play playlist");
 		MenuItem queuePlaylistOption = new MenuItem("Queue playlist");
@@ -190,7 +220,7 @@ public class Andin extends Application{
 				String playlistname = playlistView.getSelectionModel().getSelectedItem();
 				String[] mediaPaths = playlistHandler.getMediaPaths(playlistname);
 				Media[] mediaArray = buildPlaylist(mediaPaths);
-
+	
 				for(int i=0;i<mediaArray.length;i++){
 					queueMedia(mediaArray[i]);
 				}
@@ -207,7 +237,7 @@ public class Andin extends Application{
 				}
 				String playlistname=playlistView.getSelectionModel().getSelectedItem();
 				playlistHandler.addMedia(playlistname, paths);
-
+	
 			}
 		});
 		playlistView.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -219,6 +249,11 @@ public class Andin extends Application{
 				if(mouseEvent.getButton().equals(MouseButton.PRIMARY) && mouseEvent.getClickCount()==2){
 					String playlistname = playlistView.getSelectionModel().getSelectedItem();
 					playPlaylist(playlistname);
+				}
+				if(mouseEvent.getButton().equals(MouseButton.PRIMARY) && mouseEvent.getClickCount()==1){
+					String playlistName = playlistView.getSelectionModel().getSelectedItem();
+					showPlaylistContents(playlistName,playlistContentsBox);
+					playlistContentsBox.setVisible(true);
 				}
 			}
 		});
@@ -234,7 +269,7 @@ public class Andin extends Application{
 					public void handle(KeyEvent keyEvent) {
 						if (keyEvent.getCode() == KeyCode.ENTER)  {
 							String text = area.getText();
-
+	
 							playlistHandler.newPlaylist(text);
 							playlistView.getItems().add(text);
 							
@@ -248,7 +283,7 @@ public class Andin extends Application{
 					@Override
 					public void handle(ActionEvent event){
 						String text = area.getText();
-
+	
 						playlistHandler.newPlaylist(text);
 						playlistView.getItems().add(text);
 						
@@ -267,10 +302,12 @@ public class Andin extends Application{
 		playlistView.setContextMenu(contextMenu);
 		
 		playlistBox.getChildren().add(newPlaylistButton);
+
 		playlistBox.getChildren().add(playlistView);
 		playlistBox.setVgrow(playlistView, Priority.ALWAYS);
 		return playlistBox;
 	}
+
 		StackPane makeMediaPane(){
 		StackPane stackPane = new StackPane();
 
